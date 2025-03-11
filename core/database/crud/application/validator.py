6d986719c8ac0e891eef4dc2796dc12f16d0c2b1
WinerTy <nikitokmay@gmail.com>
2025-03-11 22:38:57 +0500

    Upd: Validator upd

--- core/database/crud/application/validator.py
+++ core/database/crud/application/validator.py
@@ -12,10 +12,17 @@ if TYPE_CHECKING:
 class ApplicationValidator(BaseValidator):
     def validate(self, instance: "Applications", **kwargs) -> None:
         user_id = kwargs.get("user_id")
+        self._validate_status(instance)
+        self._validate_permissions(instance, user_id)
+
+    @staticmethod
+    def _validate_status(instance: "Applications"):
+        if instance.status != ChoicesApplicationStatus.pending:
+            raise HTTPException(status_code=400, detail="Application is not pending")
+
+    @staticmethod
+    def _validate_permissions(instance: "Applications", user_id: int):
         if instance.user_id != user_id:
             raise HTTPException(
                 status_code=403, detail="You can't update this application"
             )
-
-        if instance.status != ChoicesApplicationStatus.pending:
-            raise HTTPException(status_code=400, detail="Application is not pending")
